1f0d79aeb3560dbc943c1cc44aa0ec393a0d929a
Dallin <dallinis@hotmail.com>
2020-01-01 14:53:11 -0700

    initial commit

--- tests/ex_tests.c
+++ tests/ex_tests.c
@@ -1,40 +1,1 @@
-#include <stdio.h>
-#include "dbg.h"
-#include <dlfcn.h>
-
-typedef int (*lib_function) (const char *data);
-
-int run_lib_function(char *lib_file, char *func_to_run, char *data);
-
-int main(int argc, char *argv[])
-{
-	run_lib_function("./build/libex.so", "uppercase", "hello");
-
-	return 0;
-}
-
-int run_lib_function(char *lib_file, char *func_to_run, char *data)
-{
-	int rc = 0;
-
-	void *lib = dlopen(lib_file, RTLD_NOW);
-	check(lib != NULL, "Failed to open the library %s: %s", lib_file,
-	      dlerror());
-
-	lib_function func = dlsym(lib, func_to_run);
-	check(func != NULL,
-	      "Did not find %s function in the library %s: %s", func_to_run,
-	      lib_file, dlerror());
-
-	rc = func(data);
-	check(rc == 0, "Function %s return %d for data: %s", func_to_run,
-	      rc, data);
-
-	rc = dlclose(lib);
-	check(rc == 0, "Failed to close %s", lib_file);
-
-	return 0;
-
-	error:
-	return 1;
-}
+#include "minunit.h"#include <dlfcn.h>typedef int (*lib_function) (const char *data);char *lib_file = "build/libex.so";void *lib = NULL;int check_function(const char *func_to_run, const char *data,                   int expected){	lib_function func = dlsym(lib, func_to_run);	check(func != NULL,	      "Did not find %s function in the library %s: %s", func_to_run,	      lib_file, dlerror());	int rc = func(data);	check(rc == expected, "Function %s return %d for data: %s",	      func_to_run, rc, data);	return 1;	error:		return 0;}char *test_functions(){	mu_assert(check_function("print_a_message", "Hello", 0),	          "print_a_message failed.");	mu_assert(check_function("uppercase", "Hello", 0),	          "uppercase failed.");	mu_assert(check_function("lowercase", "Hello", 0),	          "lowercase failed.");	return NULL;}char *test_failures(){	mu_assert(check_function("fail_on_purpose", "Hello", 1),	          "fail_on_purpose should fail.");	return NULL;}char *all_tests(){	mu_suite_start();	lib = dlopen(lib_file, RTLD_NOW);	mu_run_test(test_functions);	mu_run_test(test_failures);	dlclose(lib);	return NULL;}RUN_TESTS(all_tests);
